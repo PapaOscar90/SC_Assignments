776df7bf5b2412dfe2a41cef0fa1a9124490675a
P Oetinger <p.oetinger@student.rug.nl>
2017-03-03 11:47:19 +0100

    File structure update

---
+++ Assignment 4/Sup Files/reactionDiffusionD.m
@@ -0,0 +1,15 @@
+function [dFdt, dGdt] = reactionDiffusionD(F, G, s, beta)
+%reactionDiffusionD Computes time derivative of the reaction diffusion system
+
+Df = 1/4;
+Dg = 1/16;
+
+% Compute the diffusion terms
+dFdt = % TODO
+dGdt = % TODO
+
+% Compute the reaction terms
+dFdt = dFdt + % TODO
+% TODO: update dGdt
+
+end
