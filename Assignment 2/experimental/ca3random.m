dd7b4fad486473d85c722db789f61fd00ec49d5c
P Oetinger <p.oetinger@student.rug.nl>
2017-03-05 12:43:56 +0100

    Completed Assignment 3
    Ready for your part 2 so I can start report

--- Assignment 2/experimental/ca3random.m
+++ Assignment 2/experimental/ca3random.m
@@ -11,7 +11,7 @@ close all                            % close open figure windows
 clear all;                           % remove items from the workspace
 
 n=64;                                % number of cells horizontally/vertically
-p=0.2;                               % probability that a cell is alive 
+p=0.7;                               % probability that a cell is alive 
 max_gen=100;                         % maximal number of generations
 
 % Initialize matrix A 
@@ -37,14 +37,12 @@ imwrite(A, imfile);                  % write A
 % Expand matrix A to matrix A1 because of the extra borders needed
 A1=zeros(n+2,n+2);                   % initialise (n+2)x(n+2) matrix with zeroes 
 A1(2:n+1,2:n+1)=A;                   % Insert matrix A in matrix A1 
-A2=zeros(n+2,n+2);
+
 % Now compute the successive generations via the majority rule. 
 % The algorithm should terminate as soon as no more differences 
 % occur between successive generations.
-imHandle = imagesc(A1,[0 1]);        % display the matrix A1 as an image. 
-                                     % The value range of A is [0 1].
-colormap(gray);                      % set a gray scale color table
 
+% Here comes your code ....
 while gen<=max_gen
   A2 = A1;
   for j=2:n+1
@@ -52,9 +50,9 @@ while gen<=max_gen
       neighborSum=0;
       M=A2(j-1:j+1,k-1:k+1);
       neighborSum = sum(sum(M))-M(2,2);
-      if (A1(j,k) == 1 && neighborSum < 4)
+      if (A1(j,k) == 1 && neighborSum < 2 || A1(j,k) == 1 && neighborSum > 3)
         A1(j,k) = 0;
-      elseif (A1(j,k) == 0 && neighborSum > 4)
+      elseif (A1(j,k) == 0 && neighborSum ==3)
         A1(j,k) = 1;
       end
     end
@@ -68,8 +66,3 @@ while gen<=max_gen
   pause(1);
 
 end
-
-imfile = [imname,'_n=',int2str(n),'_p=',num2str(p),'_gen=',int2str(gen),'.png'];
-imwrite(A1, imfile); 
-fprintf('final fraction of living cells=%f\n',sum(sum(abs(A1)))/n^2);
-
--- Assignment 2/output/ca3.m
+++ Assignment 2/output/ca3.m
@@ -11,12 +11,13 @@ close all                            % close open figure windows
 clear all;                           % remove items from the workspace
 
 n=64;                                % number of cells horizontally/vertically
-p=0.2;                               % probability that a cell is alive 
+p=0.7;                               % probability that a cell is alive 
 max_gen=100;                         % maximal number of generations
 
 % Initialize matrix A 
-imname='random';                     % name of the pattern
-A=rand(n,n)<p;                       % n x n matrix A with random zeroes/ones
+imname='gosper';                     % name of the pattern
+inputFile = [imname, '_n=', int2str(n), '.mat'];                       % n x n matrix A with random zeroes/ones
+load(inputFile, 'A');
                                      % expected number of living cells is p (for n large)
 
 % Print the initial fraction of living cells on the screen
@@ -31,20 +32,18 @@ colormap(gray);                      % set a gray scale color table
 
 % Write the image to a PNG file
 gen=1;                               % current generation number 
-imfile = [imname,'_n=',int2str(n),'_p=',num2str(p),'_gen=',int2str(gen),'.png'];
+imfile = [imname,'_n=',int2str(n),'_gen=',int2str(gen),'.png'];
 imwrite(A, imfile);                  % write A 
 
 % Expand matrix A to matrix A1 because of the extra borders needed
 A1=zeros(n+2,n+2);                   % initialise (n+2)x(n+2) matrix with zeroes 
 A1(2:n+1,2:n+1)=A;                   % Insert matrix A in matrix A1 
-A2=zeros(n+2,n+2);
+
 % Now compute the successive generations via the majority rule. 
 % The algorithm should terminate as soon as no more differences 
 % occur between successive generations.
-imHandle = imagesc(A1,[0 1]);        % display the matrix A1 as an image. 
-                                     % The value range of A is [0 1].
-colormap(gray);                      % set a gray scale color table
 
+% Here comes your code ....
 while gen<=max_gen
   A2 = A1;
   for j=2:n+1
@@ -52,9 +51,9 @@ while gen<=max_gen
       neighborSum=0;
       M=A2(j-1:j+1,k-1:k+1);
       neighborSum = sum(sum(M))-M(2,2);
-      if (A1(j,k) == 1 && neighborSum < 4)
+      if (A1(j,k) == 1 && neighborSum < 2 || A1(j,k) == 1 && neighborSum > 3)
         A1(j,k) = 0;
-      elseif (A1(j,k) == 0 && neighborSum > 4)
+      elseif (A1(j,k) == 0 && neighborSum ==3)
         A1(j,k) = 1;
       end
     end
@@ -65,11 +64,9 @@ while gen<=max_gen
   gen++;
   set(imHandle, 'CData', A1);
   drawnow;
-  pause(1);
-
+  pause(0.1);
+  if (gen == 1 || gen == 16 || gen == 46 || gen == 76)
+    imfile = [imname,'_n=',int2str(n),'_gen=',int2str(gen),'.png'];
+    imwrite(A1, imfile); 
+  end
 end
-
-imfile = [imname,'_n=',int2str(n),'_p=',num2str(p),'_gen=',int2str(gen),'.png'];
-imwrite(A1, imfile); 
-fprintf('final fraction of living cells=%f\n',sum(sum(abs(A1)))/n^2);
-
