b824a98025145753d034b35c793236bba5c39ce4
P Oetinger <p.oetinger@student.rug.nl>
2017-03-21 20:24:30 +0100

    change

--- Practical 3/output/nbody_2.m
+++ Practical 3/output/nbody_2.m
@@ -1,7 +1,4 @@
-[times, positions, velocities] = simulate2sym([1.988544e6 5.97219], 
-                                           [0 0 0; 0.983236 0 0]',
-                                           [0 0 0; 0 0.0174939 0]',
-                                           10);
+[times, positions, velocities] = simulate2sym([1.988544e6 5.97219],[0 0 0; 0.983236 0 0]',[0 0 0; 0 0.0174939 0]',10);
 sunPositions   = squeeze(positions(1:2,1,:))';
 earthPositions = squeeze(positions(1:2,2,:))';
 plot(earthPositions(:,1), earthPositions(:,2), 'Color', 'blue'); hold on;
